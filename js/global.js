44d58433d443a1890ff1101dd4455abd101bc794
cjogloc <cjogloc@users.noreply.github.com>
2022-11-15 10:30:29 +1100

    Updated api endpoint to EU

--- js/global.js
+++ js/global.js
@@ -5,7 +5,7 @@
     // Define the Boxever settings 
     var _boxever_settings = {
         client_key: 'psfu6uh05hsr9c34rptlr06dn864cqrx', // Replace with your client key
-        target: 'https://api-ap-southeast-2-production.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
+        target: 'https://api.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
         cookie_domain: 'github.io', // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
 	web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net", // This is required for Sitecore Personalize for web experience and experiments
 	pointOfSale: "mnjKumar" // This is the point of sale you have setup in Sitecore CDP & Personalize
